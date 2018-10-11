9f8e796f1fa3d70e11c2d4c7ad3fd3848b3dad05
bgreene <bgreene@sandia.gov>
2018-10-11 16:48:44 -0600

    shoddy implementation on the backprop

--- ps5Files/nn_practice.py
+++ ps5Files/nn_practice.py
@@ -32,5 +32,23 @@ def sum_squared_error(inOuts, weights):
 #         results.append(err)
 
 # results = pd.from_dict(results)
-def compute_w2_update():
-    pass
+
+# following parameters: (1) the input to the network, (2) the target (desired) output, (3) the weights as a tuple (w1,w2)(w1,w2), and (4) the learning rate η. It should return the change that should be made to w2 given that this input/output error was just observed. Test your function in the cell below, comparing the output to one that you calculate by hand.
+
+# g′(n)=g(n)⋅(1−g(n)) 
+
+
+#############
+# REALLY NOT SURE ABOUT THIS....
+# NEED TO TAKE A GOOD LOOK...
+################
+def compute_w2_update(IN, target, weights, rate):
+    y_gWx = target - calc_output(IN, weights)
+    input_i = 1.0/(1+math.exp(weights[0]*IN))
+    
+#     g = 1.0/(1+math.exp())
+    
+    error = target - 1.0/(1+math.exp(input_i*weights[1]))
+    deriv = 1.0/(1+math.exp(input_i*weights[1])) * (1 - (1.0/(1+math.exp(input_i*weights[1]))))
+    #delta_w_2 = rate * y_gWx * 
+    return rate*error*deriv*input_i
